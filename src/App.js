4c3bed3abc146ddff5a90b845f184bdcf3c592ac
KapilRijhwani <kapil.rijhwani@gmail.com>
2019-12-26 02:27:44 +0530

    Created NavigationBar Component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import Cart from "./components/Cart";
+import Details from "./components/Details";
+import Default from "./components/Default";
 
 function App() {
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-6">column #1</div>
-        <div className="col-6">
-          <span>
-            <i className="fas fa-home" />
-          </span>
-        </div>
-      </div>
-    </div>
+    <React.Fragment>
+      <h3>Title</h3>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={ProductList} />
+        <Route path="/details" component={Details} />
+        <Route path="/cart" component={Cart} />
+        <Route component={Default} />
+      </Switch>
+    </React.Fragment>
   );
 }
 
---
+++ src/components/ProductList.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Product from "./Product";
+
+export default function ProductList() {
+  return (
+    <div>
+      <h3>Hello from product list</h3>
+      <Product />
+    </div>
+  );
+}
