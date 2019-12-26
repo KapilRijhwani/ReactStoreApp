1d8afcd31b684e5d9571acee01ffda80abfa6cb1
KapilRijhwani <kapil.rijhwani@gmail.com>
2019-12-26 21:55:44 +0530

    Created Details page of product

--- src/context.js
+++ src/context.js
@@ -7,12 +7,27 @@ const ProductContext = React.createContext();
 
 class ProductProvider extends Component {
   state = {
-    products: storeProducts,
+    products: [],
     detailProduct
   };
 
-  addToCart = () => {
-    console.log("hello from addtoCart");
+  componentDidMount() {
+    this.setProducts();
+  }
+
+  setProducts = () => {
+    let tempProducts = [];
+    storeProducts.forEach(item => {
+      const singleItem = { ...item };
+      tempProducts = [...tempProducts, singleItem];
+    });
+    this.setState(() => {
+      return { products: tempProducts };
+    });
+  };
+
+  addToCart = id => {
+    console.log(`hello from addtoCart.Id is ${id}`);
   };
 
   handleDetail = () => {
