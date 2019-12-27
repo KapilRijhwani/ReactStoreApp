feaecf3d1f89ab6a95ae6aedcc12ee95259bec3f
KapilRijhwani <kapil.rijhwani@gmail.com>
2019-12-27 14:02:04 +0530

    Updates

---
+++ precache-manifest.caeafc37b387211ce8ede0210fed9b90.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37bc0f4cff77c80518676c00a5b52aa5",
+    "url": "/ReactStoreApp/index.html"
+  },
+  {
+    "revision": "d4ae101743482d071f6e",
+    "url": "/ReactStoreApp/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "413af522efe9a3440b34",
+    "url": "/ReactStoreApp/static/css/main.3e701f56.chunk.css"
+  },
+  {
+    "revision": "d4ae101743482d071f6e",
+    "url": "/ReactStoreApp/static/js/2.39dc4dc8.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/ReactStoreApp/static/js/2.39dc4dc8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "413af522efe9a3440b34",
+    "url": "/ReactStoreApp/static/js/main.60146566.chunk.js"
+  },
+  {
+    "revision": "b137ceb11c8fb76b158b",
+    "url": "/ReactStoreApp/static/js/runtime-main.94ea6b21.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/ReactStoreApp/static/media/logo.9f7e506d.svg"
+  }
+]);
